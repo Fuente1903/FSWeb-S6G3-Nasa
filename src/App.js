21f135ea8552cada9179b160f597ecbf62917f09
Fuente1903 <125641774+Fuente1903@users.noreply.github.com>
2023-05-22 12:20:01 +0300

    Güncelleme
    Güncelleme yapıldı.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import { dummyData } from "./component/dummyData";
+import axios from "axios";
+import Nasa from "./component/Nasa";
 
 function App() {
+  const [data, setData] = useState([dummyData]);
+  const [date, setDate] = useState("2023-01-07");
+
+  useEffect(() => {
+    axios
+      .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=" + date)
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => console.log(error));
+  }, [date]);
+
+  function changeDate(day) {
+    let formattedDate = new Date(date);
+    formattedDate.setDate(formattedDate.getDate() + day);
+    let oldFormatDate = `${formattedDate.getFullYear()}-${formattedDate.getMonth() +
+      1}-${formattedDate.getDay()}`;
+    setDate(oldFormatDate);
+  }
+
   return (
     <div className="App">
-      <p>
-        NASA uygulamasını yapmak için README.md dosyasıdaki talimatları takip edin
-		İyi eğlenceler! <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <Nasa data={data} changeDate={changeDate} />
     </div>
   );
 }
